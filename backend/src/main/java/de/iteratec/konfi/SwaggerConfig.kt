11f4da7d0c72d745ab320e86f2143425fb4b3ced
Marc Reichelt <marc.reichelt@iteratec.com>
2021-02-08 16:46:20 +0100

    Convert SwaggerConfig to Kotlin

--- backend/src/main/java/de/iteratec/konfi/SwaggerConfig.kt
+++ backend/src/main/java/de/iteratec/konfi/SwaggerConfig.kt
@@ -1,20 +1,19 @@
-package de.iteratec.konfi;
+package de.iteratec.konfi
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
 
 @Configuration
-public class SwaggerConfig {
-
+class SwaggerConfig {
     @Bean
-    public Docket demoApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(path -> !path.matches("/error.*"))
-                .build();
+    fun demoApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths { path: String -> !path.matches(Regex("/error.*")) }
+            .build()
     }
 }
