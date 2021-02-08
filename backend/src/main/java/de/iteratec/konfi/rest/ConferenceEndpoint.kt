49aee5ab488647b758d278f3cd476d805def51a2
Marc Reichelt <marc.reichelt@iteratec.com>
2021-02-08 17:19:03 +0100

    Convert ConferenceEndpoint to Kotlin

--- backend/src/main/java/de/iteratec/konfi/rest/ConferenceEndpoint.kt
+++ backend/src/main/java/de/iteratec/konfi/rest/ConferenceEndpoint.kt
@@ -1,65 +1,56 @@
-package de.iteratec.konfi.rest;
+package de.iteratec.konfi.rest
 
-import de.iteratec.konfi.rest.dto.AttendeeDto;
-import de.iteratec.konfi.rest.dto.ConferenceDto;
-import de.iteratec.konfi.domain.ConferenceService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
+import de.iteratec.konfi.domain.ConferenceService
+import io.swagger.annotations.ApiOperation
+import de.iteratec.konfi.rest.dto.ConferenceDto
+import javax.validation.Valid
+import de.iteratec.konfi.rest.dto.AttendeeDto
+import io.swagger.annotations.Api
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/conference")
-@Api(value = "Conference", tags = { "Conference" }, consumes = "application/json", produces = "application/json")
-public class ConferenceEndpoint {
-
-    private ConferenceService conferenceService;
-
-    public ConferenceEndpoint(ConferenceService conferenceService) {
-        this.conferenceService = conferenceService;
-    }
-
+@Api(value = "Conference", tags = ["Conference"], consumes = "application/json", produces = "application/json")
+class ConferenceEndpoint(private val conferenceService: ConferenceService) {
     @GetMapping
     @ApiOperation(value = "List all conferences")
-    public List<ConferenceDto> findAll() {
-        return conferenceService.findAll();
+    fun findAll(): List<ConferenceDto> {
+        return conferenceService.findAll()
     }
 
     @GetMapping("/{id}")
     @ApiOperation(value = "Get a single conference")
-    public ConferenceDto findOne(@PathVariable Long id) {
-        return conferenceService.findOne(id);
+    fun findOne(@PathVariable id: Long?): ConferenceDto {
+        return conferenceService.findOne(id)
     }
 
     @PostMapping
     @ApiOperation(value = "Create a new conference")
-    public ConferenceDto create(@Valid @RequestBody ConferenceDto dto) {
-        return conferenceService.create(dto);
+    fun create(@RequestBody dto: @Valid ConferenceDto?): ConferenceDto {
+        return conferenceService.create(dto)
     }
 
     @DeleteMapping("/{id}")
     @ApiOperation(value = "Delete an existing conference")
-    public void delete(@PathVariable Long id) {
-        conferenceService.delete(id);
+    fun delete(@PathVariable id: Long?) {
+        conferenceService.delete(id)
     }
 
     @GetMapping("/{id}/attendees")
     @ApiOperation(value = "Get a list of conference's attendees")
-    public List<AttendeeDto> getAttendees(@PathVariable Long id) {
-        return conferenceService.getAttendees(id);
+    fun getAttendees(@PathVariable id: Long?): List<AttendeeDto> {
+        return conferenceService.getAttendees(id)
     }
 
     @PostMapping("/{id}/attendees")
     @ApiOperation(value = "Register a new attendee")
-    public void register(@PathVariable Long id, @Valid @RequestBody AttendeeDto attendee) {
-        conferenceService.register(id, attendee);
+    fun register(@PathVariable id: Long?, @RequestBody attendee: @Valid AttendeeDto?) {
+        conferenceService.register(id, attendee)
     }
 
     @DeleteMapping("/{id}/attendees")
     @ApiOperation(value = "Remove an existing attendee")
-    public void deregister(@PathVariable Long id, @Valid @RequestBody AttendeeDto attendee) {
-        conferenceService.deregister(id, attendee);
+    fun deregister(@PathVariable id: Long?, @RequestBody attendee: @Valid AttendeeDto?) {
+        conferenceService.deregister(id, attendee)
     }
 }
