d539f5dc4a4846b2fb35830d79eafb9de08d9611
Marc Reichelt <marc.reichelt@iteratec.com>
2021-02-08 16:59:23 +0100

    Convert DtoMapper to Kotlin

--- backend/src/main/java/de/iteratec/konfi/rest/DtoMapper.kt
+++ backend/src/main/java/de/iteratec/konfi/rest/DtoMapper.kt
@@ -1,47 +1,46 @@
-package de.iteratec.konfi.rest;
+package de.iteratec.konfi.rest
 
-import de.iteratec.konfi.rest.dto.AttendeeDto;
-import de.iteratec.konfi.rest.dto.ConferenceDto;
-import de.iteratec.konfi.domain.model.Attendee;
-import de.iteratec.konfi.domain.model.Conference;
-import org.springframework.stereotype.Component;
+import de.iteratec.konfi.rest.dto.ConferenceDto
+import de.iteratec.konfi.domain.model.Conference
+import de.iteratec.konfi.rest.dto.AttendeeDto
+import de.iteratec.konfi.domain.model.Attendee
+import org.springframework.stereotype.Component
 
 @Component
-public class DtoMapper {
-
-    public Conference toModel(ConferenceDto dto) {
-        Conference model = new Conference();
-        model.setId(dto.getId());
-        model.setName(dto.getName());
-        model.setMaxAttendees(dto.getMaxAttendees());
-        model.setStartDate(dto.getStartDate());
-        model.setEndDate(dto.getEndDate());
-        return model;
+class DtoMapper {
+    fun toModel(dto: ConferenceDto): Conference {
+        val model = Conference()
+        model.id = dto.id
+        model.name = dto.name
+        model.maxAttendees = dto.maxAttendees
+        model.startDate = dto.startDate
+        model.endDate = dto.endDate
+        return model
     }
 
-    public ConferenceDto toDto(Conference model) {
-        ConferenceDto dto = new ConferenceDto();
-        dto.setId(model.getId());
-        dto.setName(model.getName());
-        dto.setMaxAttendees(model.getMaxAttendees());
-        dto.setStartDate(model.getStartDate());
-        dto.setEndDate(model.getEndDate());
-        return dto;
+    fun toDto(model: Conference): ConferenceDto {
+        val dto = ConferenceDto()
+        dto.id = model.id
+        dto.name = model.name
+        dto.maxAttendees = model.maxAttendees
+        dto.startDate = model.startDate
+        dto.endDate = model.endDate
+        return dto
     }
 
-    public Attendee toModel(AttendeeDto dto) {
-        Attendee model = new Attendee();
-        model.setFirstName(dto.getFirstName());
-        model.setLastName(dto.getLastName());
-        model.setEmail(dto.getEmail());
-        return model;
+    fun toModel(dto: AttendeeDto): Attendee {
+        val model = Attendee()
+        model.firstName = dto.firstName
+        model.lastName = dto.lastName
+        model.email = dto.email
+        return model
     }
 
-    public AttendeeDto toDto(Attendee model) {
-        AttendeeDto dto = new AttendeeDto();
-        dto.setFirstName(model.getFirstName());
-        dto.setLastName(model.getLastName());
-        dto.setEmail(model.getEmail());
-        return dto;
+    fun toDto(model: Attendee): AttendeeDto {
+        val dto = AttendeeDto()
+        dto.firstName = model.firstName
+        dto.lastName = model.lastName
+        dto.email = model.email
+        return dto
     }
 }
