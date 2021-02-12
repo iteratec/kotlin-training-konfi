20e03644a1db615a00e9557a106e725f3b7e274f
Marc Reichelt <marc.reichelt@iteratec.com>
2021-02-12 08:09:47 +0100

    Convert Conference to Kotlin

--- backend/src/main/java/de/iteratec/konfi/domain/model/Conference.kt
+++ backend/src/main/java/de/iteratec/konfi/domain/model/Conference.kt
@@ -1,77 +1,24 @@
-package de.iteratec.konfi.domain.model;
+package de.iteratec.konfi.domain.model
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime
+import de.iteratec.konfi.domain.model.Attendee
+import de.iteratec.konfi.domain.model.Conference
+import java.util.ArrayList
+import javax.persistence.*
 
 @Entity
-public class Conference {
-
+class Conference {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;
-
-    private int maxAttendees;
-
-    private LocalDateTime startDate;
-
-    private LocalDateTime endDate;
+    var id: Long? = null
+    var name: String? = null
+    var maxAttendees = 0
+    var startDate: LocalDateTime? = null
+    var endDate: LocalDateTime? = null
 
     @ElementCollection
-    private List<Attendee> attendees = new ArrayList<>();
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getMaxAttendees() {
-        return maxAttendees;
-    }
-
-    public void setMaxAttendees(int maxAttendees) {
-        this.maxAttendees = maxAttendees;
-    }
-
-    public LocalDateTime getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDateTime startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDateTime getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDateTime endDate) {
-        this.endDate = endDate;
-    }
-
-    public List<Attendee> getAttendees() {
-        return attendees;
-    }
-
-    public void setAttendees(List<Attendee> attendees) {
-        this.attendees = attendees;
-    }
-
-    public boolean collidesWith(Conference other) {
-        return other.endDate.compareTo(startDate) >= 0 && other.startDate.compareTo(endDate) <= 0;
+    var attendees: List<Attendee> = ArrayList()
+    fun collidesWith(other: Conference): Boolean {
+        return other.endDate!!.compareTo(startDate) >= 0 && other.startDate!!.compareTo(endDate) <= 0
     }
 }
