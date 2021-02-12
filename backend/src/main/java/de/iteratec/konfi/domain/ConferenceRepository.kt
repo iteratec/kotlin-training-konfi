c4de8ff00c0d8abdb64b8fbb42d444d4bbbf958a
Marc Reichelt <marc.reichelt@iteratec.com>
2021-02-12 08:19:44 +0100

    Convert ConferenceRepository to Kotlin

--- backend/src/main/java/de/iteratec/konfi/domain/ConferenceRepository.kt
+++ backend/src/main/java/de/iteratec/konfi/domain/ConferenceRepository.kt
@@ -1,11 +1,8 @@
-package de.iteratec.konfi.domain;
+package de.iteratec.konfi.domain
 
-import de.iteratec.konfi.domain.model.Conference;
-import org.springframework.data.jpa.repository.JpaRepository;
+import de.iteratec.konfi.domain.model.Conference
+import org.springframework.data.jpa.repository.JpaRepository
 
-import java.util.List;
-
-public interface ConferenceRepository extends JpaRepository<Conference, Long> {
-
-    List<Conference> findByName(String name);
+interface ConferenceRepository : JpaRepository<Conference, Long> {
+    fun findByName(name: String): List<Conference>
 }
