acf752cedacbd61c995db1fff14e0cbc2b31eb23
Marc Reichelt <marc.reichelt@iteratec.com>
2021-02-12 08:11:12 +0100

    Make Conference a data class

--- backend/src/main/java/de/iteratec/konfi/domain/model/Conference.kt
+++ backend/src/main/java/de/iteratec/konfi/domain/model/Conference.kt
@@ -1,24 +1,24 @@
 package de.iteratec.konfi.domain.model
 
 import java.time.LocalDateTime
-import de.iteratec.konfi.domain.model.Attendee
-import de.iteratec.konfi.domain.model.Conference
-import java.util.ArrayList
+import java.util.*
 import javax.persistence.*
 
 @Entity
-class Conference {
+data class Conference(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
-    var name: String? = null
-    var maxAttendees = 0
-    var startDate: LocalDateTime? = null
-    var endDate: LocalDateTime? = null
-
+    var id: Long? = null,
+    var name: String? = null,
+    var maxAttendees: Int = 0,
+    var startDate: LocalDateTime? = null,
+    var endDate: LocalDateTime? = null,
     @ElementCollection
-    var attendees: List<Attendee> = ArrayList()
+    var attendees: List<Attendee> = ArrayList(),
+) {
+
     fun collidesWith(other: Conference): Boolean {
         return other.endDate!!.compareTo(startDate) >= 0 && other.startDate!!.compareTo(endDate) <= 0
     }
+
 }
