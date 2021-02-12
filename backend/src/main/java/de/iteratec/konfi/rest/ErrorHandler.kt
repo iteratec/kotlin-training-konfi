00f8b5aa193a801ddbb751e6d022925e5280f902
Marc Reichelt <marc.reichelt@iteratec.com>
2021-02-12 07:25:47 +0100

    Convert ErrorHandler to Kotlin

--- backend/src/main/java/de/iteratec/konfi/rest/ErrorHandler.kt
+++ backend/src/main/java/de/iteratec/konfi/rest/ErrorHandler.kt
@@ -1,18 +1,17 @@
-package de.iteratec.konfi.rest;
+package de.iteratec.konfi.rest
 
-import de.iteratec.konfi.domain.model.ConferenceCollisionException;
-import de.iteratec.konfi.domain.model.RegisterAttendeeException;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import de.iteratec.konfi.domain.model.ConferenceCollisionException
+import de.iteratec.konfi.domain.model.RegisterAttendeeException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
 
 @ControllerAdvice
-public class ErrorHandler {
-
-    @ExceptionHandler(value = {ConferenceCollisionException.class, RegisterAttendeeException.class})
+class ErrorHandler {
+    @ExceptionHandler(value = [ConferenceCollisionException::class, RegisterAttendeeException::class])
     @ResponseBody
-    public ResponseEntity<String> handle(Exception ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+    fun handle(ex: Exception): ResponseEntity<String?> {
+        return ResponseEntity.badRequest().body(ex.message)
     }
 }
